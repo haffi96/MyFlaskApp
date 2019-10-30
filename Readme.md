e6d2bf0874a6ff26c85d192ba5169b18c0b8a1d2
Haffi Mazhar <haffimazhar96@gmail.com>
2019-10-30 16:02:41 +0000

    Added comments and readme, also a minor change to name class in app.py

---
+++ Readme.md
@@ -0,0 +1,11 @@
+# MyFlaskApp
+
+This app was created using Python3 and Flask packages
+
+1. Created virtual env in working directory in cmd using: 'py -m venv venv'
+2. Activate venv: 'venv\Scripts\activate'
+3. Then, install flask: 'pip install flask'
+4. Set up the python script:'export FLASK_APP=app.py, set FLASK_APP=app.py'
+5. To ensure debugging, used command: 'set FLASK_ENV = development'
+6. Then after writing the python script and html templates, used command 'flask run' to run the server
+7. The server was then accessed through the address given in cmd.
--- app.py
+++ app.py
@@ -1,22 +1,31 @@
 from flask import Flask, render_template, request, redirect, url_for
+# to import the Flask class and then added the required functions needed as I went along
 from wtforms import Form, StringField, TextAreaField, validators
+# Used wtforms to create the input field as it makes the code alot simpler and cleaner. Also easier to understand
 from wtforms.validators import InputRequired
+# This was used to import the function needed for validating input.
 
 app = Flask(__name__)
-app.config.from_object(__name__)
 app.config['SECRET_KEY'] = 'FlaskApp'
+# I have added the secret key which could also be a randomzied string as I understand that this is good practice however I am not sure how this works
 
 
-class NameEntry(Form):
-    name = StringField('Name:', validators=[InputRequired()])
+class NameEntry(Form):  # this is to generate a class for the name field
+    name = StringField(validators=[InputRequired()])
+    # StringField calls on the function imported at the start and
+    # I have called this 'name' which is used below in the logic part of the script
+    # Validators is used to ensure an input is given
 
 
 @app.route('/', methods=['GET', 'POST'])
+# GET and POST are methods used to handle data. i.e GET may be used to retrieve informaiton from a database
 def home():
     form = NameEntry(request.form)
     if request.method == 'POST':
         name = request.form['name']
+        # this links to the page you will be redirected to after submitting
         return render_template('name.html', name=name)
+    # this is template used to markup the html displayed on the home page
     return render_template('home.html', form=form)
 
 
